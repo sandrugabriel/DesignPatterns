7bbfac7844b24eb6208d7e7c1e54cffd59bdd92f
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-01 17:36:17 +0300

    done

---
+++ SOLID-Principles/SOLID-Principles/Single-Responsibility-Principle/With/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principles.Single_Responsibility_Principle.With
+{
+    public class User
+    {
+
+        private readonly string _email;
+        private readonly string _password;
+
+        public User(string email, string password)
+        {
+            _email = email;
+            _password = password;
+        }
+
+
+    }
+}
