424858d3281c0a17d4882f825b036a30ec51aa5e
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-03 23:20:33 +0300

    done

---
+++ The-Singleton-Pattern/The-Singleton-Pattern/Ex1/WeldingArmPoolSingleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Singleton_Pattern.Ex1
+{
+    public sealed class WeldingArmPoolSingleton
+    {
+        private static WeldingArmPoolSingleton _instance;
+        private int _maxSize = 10;
+
+        private WeldingArmPoolSingleton(){ }
+
+        public static WeldingArmPoolSingleton Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new WeldingArmPoolSingleton();
+                return _instance;
+            }
+        }
+    }
+
+}
---
+++ The-Singleton-Pattern/The-Singleton-Pattern/Program.cs
@@ -0,0 +1,10 @@
+﻿using The_Singleton_Pattern.Ex2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Singleton singleton = Singleton.Instance;
+        singleton.doSometimes();
+    }
+}
