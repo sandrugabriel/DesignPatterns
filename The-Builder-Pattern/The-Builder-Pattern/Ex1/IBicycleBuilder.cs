3810d01f33387b2126bae0b9041c4f12f251d60c
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-04 16:09:24 +0300

    done

---
+++ The-Builder-Pattern/The-Builder-Pattern/Ex1/IBicycleBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_Builder_Pattern.Ex1
+{
+    interface IBicycleBuilder
+    {
+        public void Reset();
+        public void BuildFrame();
+        public void BuildHandleBars();
+        public void BuildSeat();
+        public void BuildSuspension();
+        public void BuildDriveTrain();
+        public void BuildBrakes();
+        IBicycleProduct GetProduct();
+    }
+
+}
