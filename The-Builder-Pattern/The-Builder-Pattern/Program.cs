facec8c3296ae284cd9c158d95199d4bac7fd8cc
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-11 15:02:52 +0300

    done

--- The-Builder-Pattern/The-Builder-Pattern/Program.cs
+++ The-Builder-Pattern/The-Builder-Pattern/Program.cs
@@ -1,8 +1,24 @@
 ﻿
+using The_Builder_Pattern.Models;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
+
+
+        User user = IUserBuilder.build()
+            .Id(1)
+            .Parola("test1234")
+            .Nume("test")
+            .Email("test@mail.com");
+
+
+        User user1 = IUserBuilder.build()
+            .Id(2)
+            .Parola("test21234")
+            .Nume("test2")
+            .Email("test2@mail.com");
 
 
 
