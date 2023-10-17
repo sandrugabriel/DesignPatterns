77e57a50e7f26d74083902f8eebd43164aecde18
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-17 22:45:57 +0300

    done

---
+++ Creating-a-Custom-Container/Creating-a-Custom-Container/ClassA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_a_Custom_Container
+{
+    public  class ClassA : IInterfaceA
+    {
+       public void doA()
+        {
+            Console.WriteLine("A");
+        }
+    }
+}
---
+++ Creating-a-Custom-Container/Creating-a-Custom-Container/ClassB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_a_Custom_Container
+{
+    public class ClassB : IInterfaceB
+    {
+
+        private IInterfaceA _classA;
+
+        public ClassB(IInterfaceA classA)
+        {
+            _classA = classA;
+        }
+
+        public void doB()
+        {
+            _classA.doA();
+            Console.WriteLine("B");
+        }
+    }
+}
