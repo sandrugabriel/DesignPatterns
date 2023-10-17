13340634f5f4115999da119e22fa875903e92db1
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-17 21:27:04 +0300

    done

---
+++ Loose-Coupling/Loose-Coupling/GameBoard.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loose_Coupling
+{
+    public class GameBoard
+    {
+
+        private PrimaryPlayer _player;
+
+        public GameBoard()
+        {
+
+            _player = PrimaryPlayer.Instance;
+            _player.Weapon = new Sword(12, 8);
+        }
+
+        public void PlayArea(int lvl)
+        {
+            if (lvl == 1)
+            {
+                PlayFistLevel();
+            }
+        }
+
+        public void PlayFistLevel()
+        {
+            int lvl = 1;
+            List<IEnemy> enemies = new List<IEnemy>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                enemies.Add(EnemyFactory.SpawnZombie(lvl));
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                enemies.Add(EnemyFactory.SpawnWereWolf(lvl));
+            }
+
+            foreach (var enemy in enemies)
+            {
+
+                while(enemy.Health > 0 || _player.Health > 0)
+                {
+                    _player.Weapon.Use(enemy);
+                    enemy.Attack(_player);
+                }
+            }
+
+        }
+    }
+}
---
+++ Loose-Coupling/Loose-Coupling/PrimaryPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loose_Coupling
+{
+    public class PrimaryPlayer
+    {
+        private static readonly PrimaryPlayer _instante;
+
+        private PrimaryPlayer() { }
+
+        public IWeapon Weapon { get; set; }
+
+        static PrimaryPlayer()
+        {
+            _instante = new PrimaryPlayer()
+        }
+
+        public static PrimaryPlayer Instance { get { return _instante; } }
+
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public int Armor {  get; set; }
+        public int Health {  get; set; }
+    }
+}
