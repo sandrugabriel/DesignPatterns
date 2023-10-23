0451ce7437f35f6e7e2285ab866778724d2a159c
Sandru Gabriel <gabi200611@yahoo.com>
2023-10-23 17:55:52 +0300

    done

--- Loose-Coupling/Api/Services/CardService.cs
+++ Loose-Coupling/Api/Services/CardService.cs
@@ -8,13 +8,13 @@ namespace Api.Services
         {
             return new List<Card>()
             {
-                new Card() {Attack =90,Defense=70,Name="Ultimate Shadow Wraith"},
-                new Card() {Attack =60,Defense=60,Name="a"},
-                new Card() {Attack =30,Defense=50,Name="b"},
-                new Card() {Attack =50,Defense=40,Name="c"},
-                new Card() {Attack =70,Defense=30,Name="d"},
-                new Card() {Attack =90,Defense=20,Name="f"},
-                new Card() {Attack =100,Defense=10,Name="h"},
+                new Card() {Attack1 =90,Defense1=70,Name1="Ultimate Shadow Wraith"},
+                new Card() {Attack1 = 60, Defense1 = 60,Name1="a"},
+                new Card() {Attack1 = 30, Defense1 = 50,Name1="b"},
+                new Card() {Attack1 = 50, Defense1 = 40, Name1 = "c"},
+                new Card() {Attack1 = 70, Defense1 = 30, Name1 = "d"},
+                new Card() {Attack1 = 90, Defense1 = 20, Name1 = "f"},
+                new Card() {Attack1 = 100, Defense1 = 10, Name1 = "h"},
 
             };
         }
--- Loose-Coupling/Loose-Coupling/Program.cs
+++ Loose-Coupling/Loose-Coupling/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using Built_in_ASP_IoC_Container;
+using System.Diagnostics;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       TestDecorators();
+    }
+
+    private static void TestDecorators()
+    {
+        Card soldier = new Card("Soldier", 25, 20);
+        soldier = new AttackDecorator(soldier,"Sword",15);
+        soldier = new AttackDecorator(soldier, "Amulet", 5);
+        soldier = new DefenceDecorator(soldier, "Helmet", 10);
+        soldier = new DefenceDecorator(soldier, "Heavy Armor", 45);
+
+        Console.WriteLine($"Final Status: {soldier.Attack} / {soldier.Defense}");
     }
 }
